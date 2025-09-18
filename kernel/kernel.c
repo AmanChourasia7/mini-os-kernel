892763f33a84cde2c711ea652d306af03a04af9d
Aman Chourasia <76176138+AmanChourasia7@users.noreply.github.com>
2025-09-18 13:22:22 +0530

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,14 @@
-#include "../drivers/vga.h"
-#include "../drivers/keyboard.h"
+#include <stdint.h>
 
-void kernel_main() {
-    vga_clear();
-    vga_print("Mini OS Kernel Booted!\n");
-    while(1) {
-        char c = keyboard_get_char();
-        if(c) {
-            vga_putchar(c);
-        }
+#define VGA_ADDRESS 0xB8000
+#define WHITE_ON_BLACK 0x0F
+
+void kmain() {
+    volatile uint16_t* vga = (uint16_t*)VGA_ADDRESS;
+    const char* msg = "Hello, Mini OS!";
+    for (int i = 0; msg[i] != '\0'; i++) {
+        vga[i] = (uint16_t)msg[i] | (WHITE_ON_BLACK << 8);
     }
+
+    while (1) { __asm__ volatile("hlt"); }
 }
